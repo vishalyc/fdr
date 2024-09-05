8a2bac560d9714e579eed68727fff0885bf8d4e2
Vishal <vishalyc@hotmail.com>
2024-09-04 21:57:21 -0500

    updates

--- lib/lambda-sqs-service.ts
+++ lib/lambda-sqs-service.ts
@@ -2,17 +2,13 @@ import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import { SnsService } from './sns-service';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 import * as logs from 'aws-cdk-lib/aws-logs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 import { createAlarm } from './alarms';
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as path from 'path';
-import * as fs from 'fs';
-import * as child_process from 'child_process';
 
 export class LambdaSQSService extends Construct {
     communicationsManagerQueName = 'communicationsManagerQueue';
@@ -40,74 +36,49 @@ export class LambdaSQSService extends Construct {
         // VPC for Lambda
         const vpc = new ec2.Vpc(this, 'LambdaVPC');
 
-        // S3 bucket for Lambda code
-        const bucket = new s3.Bucket(this, 'LambdaCodeBucket');
-        // Build and bundle the Lambda function using esbuild
-        const lambdaCodeDir = path.join(__dirname, '../src/lambda');
-        const outDir = path.join(__dirname, '../dist/lambda');
-        const zipFilePath = path.join(outDir, 'handler.zip');
-
-        // Create the dist directory if it doesn't exist
-        if (!fs.existsSync(outDir)){
-            fs.mkdirSync(outDir, { recursive: true });
-        }
-
-        // Bundle all .js files in the src/lambda directory
-        child_process.execSync(`esbuild ${lambdaCodeDir}/*.js --bundle --platform=node --target=node18 --external:aws-sdk --outdir=${outDir} --format=cjs`);
-
-        // Zip the entire bundled Lambda code directory
-        child_process.execSync(`zip -r ${zipFilePath} .`, { cwd: outDir });
-        
-        // Deploy Lambda code to S3 bucket
-        const deployment = new s3deploy.BucketDeployment(this, 'DeployLambdaCode', {
-            sources: [s3deploy.Source.asset(outDir)],
-            destinationBucket: bucket,
-            destinationKeyPrefix: 'lambda/', // optional prefix in the bucket
-        });
-
-        // Lambda Function
-        const fn = new lambda.Function(this, this.cmLambdaFunctionName, {
-            runtime: lambda.Runtime.NODEJS_18_X,
-            code: lambda.Code.fromBucket(bucket, 'lambda/handler.zip'),
-            handler: 'handler.handler',
-            vpc,
-            environment: {
-                QUEUE_URL: queue.queueUrl,
+        // Define the Lambda function using NodejsFunction, which automatically bundles dependencies
+        const fn = new lambda.Function(this, 'FDRLambdaFunction', {
+            runtime: cdk.aws_lambda.Runtime.NODEJS_18_X, // Define the Lambda runtime version
+            code: lambda.Code.fromAsset(path.join(__dirname, '../src/lambda')), // Path to your Lambda function
+            handler: 'handler.handler', // Name of the exported handler function in the file
+            memorySize: 1024, // Optional: Set memory size
+            timeout: cdk.Duration.seconds(300), // Optional: Set the Lambda timeout
+            vpc: vpc, // Attach the Lambda function to the VPC
+            vpcSubnets: {
+                subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS, // Specify the type of subnets the Lambda can run in
             },
-            reservedConcurrentExecutions: 10,
-            memorySize: 1024,
-            timeout: cdk.Duration.seconds(300),
+            securityGroups: [new ec2.SecurityGroup(this, 'LambdaSecurityGroup', {
+                vpc,
+                allowAllOutbound: true, // Allow outbound traffic
+            })],
         });
-
-        // Ensure Lambda depends on the S3 deployment
-        fn.node.addDependency(deployment);
 
         // Grant Lambda permissions to read from SQS
         queue.grantConsumeMessages(fn);
 
         // Use SqsEventSource to trigger Lambda from SQS
         fn.addEventSource(new SqsEventSource(queue, {
-        batchSize: 1,
+            batchSize: 1,
         }));
 
         // CloudWatch Logs for Lambda
         const logGroup = new logs.LogGroup(this, 'lambdaLogGroup', {
-        logGroupName: `/aws/lambda/${fn.functionName}`,
-        retention: logs.RetentionDays.ONE_WEEK,
+            logGroupName: `/aws/lambda/${fn.functionName}`,
+            retention: logs.RetentionDays.ONE_WEEK,
         });
 
         new cloudwatch.Alarm(this, 'sqsQueueAlarm', {
-        metric: queue.metricApproximateAgeOfOldestMessage(),
-        threshold: 300,
-        evaluationPeriods: 1,
-        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
+            metric: queue.metricApproximateAgeOfOldestMessage(),
+            threshold: 300,
+            evaluationPeriods: 1,
+            comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD,
         });
 
         new cloudwatch.Alarm(this, 'lambdaErrorAlarm', {
-        metric: fn.metricErrors(),
-        threshold: 1,
-        evaluationPeriods: 1,
-        comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+            metric: fn.metricErrors(),
+            threshold: 1,
+            evaluationPeriods: 1,
+            comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
         });
 
         var baseConfig = {
@@ -117,6 +88,7 @@ export class LambdaSQSService extends Construct {
             snsTopicArn: props.topicArn,
             statistic: 'Sum',
         };
+        this.CreateLambdaAlarms (baseConfig);
 
         // setup alarms for SQS function
         baseConfig = {
@@ -126,9 +98,10 @@ export class LambdaSQSService extends Construct {
             snsTopicArn: props.topicArn,
             statistic: 'Maximum',
         };
+        this.CreateSQSAlarms (baseConfig);
     }
 
-    public CreateSQSAlarms (baseConfig: any, cmalarmsManagerInstance: any) {
+    public CreateSQSAlarms (baseConfig: any) {
         createAlarm ({
             ...baseConfig,
             metricName: 'ApproximateAgeOfOldestMessage',
