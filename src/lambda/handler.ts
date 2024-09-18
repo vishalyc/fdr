a410d96b64dea4f1567278966db1358b2c328df0
Vishal <vishalyc@hotmail.com>
2024-09-18 08:56:26 -0500

    update

--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -3,18 +3,18 @@ import { getApplicationIdByName, updateOrCreateEndpoint } from './pinpoint';
 
 export const handler = async (event: SQSEvent) => {
     try {
+        console.info('Creating/Updating Pinpoint Endpoint:', JSON.stringify(event, null, 2));
+        // Step 1: Get the applicationId from the project name
+        const projectName = "fdr";
+        
+        const appId = await getApplicationIdByName(projectName);
+
         for (const record of event.Records) {
             const body = JSON.parse(record.body);
-
-            const projectName = "fdr";
             const recipientId = body.recipientId;
             const vendorId = body.vendorId;
             const endpointId = `${recipientId}-${vendorId}`; // Unique identifier
             const eventType = body.type;
-
-            // Step 1: Get the applicationId from the project name
-            const appId = await getApplicationIdByName(projectName);
-
             // Step 2: Create or update the endpoint
             await updateOrCreateEndpoint(
                 appId,
@@ -27,6 +27,7 @@ export const handler = async (event: SQSEvent) => {
                 body.attestationUrl,
                 eventType
             );
+            console.info(`Creating/Updating Pinpoint Endpoint for User: ${body.recipientEmail}`);
         }
     } catch (error) {
         console.error("Error handling endpoint:", error);
